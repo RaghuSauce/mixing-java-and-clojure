0ee37c0be9488126fe5362d1a27a492bd003cd57
Dantuluri <dantuluri.raghu@gmail.com>
2019-02-05 08:59:36 -0500

    added groovyc dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject mixing-java-and-clojure "1.0.0-SNAPSHOT"
   :description "A simple demonstration of Java interop and build management using javac"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [lein-groovyc "0.2.1"]
+                 ]
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
+  :groovyc-source-path "src/groovy"
   :main com.quephird.mjac.bar
   :aot [com.quephird.mjac.bar])
